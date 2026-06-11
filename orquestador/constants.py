c557ff01e4a7147cca498816fe3958c7a7ce6d2b
josemza <josemza27@gmail.com>
2026-06-11 13:26:01 -0500

    Add MAX_BYTES constant to constants.py
    Added MAX_BYTES constant for VARCHAR2 SQL columns.

--- orquestador/constants.py
+++ orquestador/constants.py
@@ -2,3 +2,4 @@ MAX_OUTPUT_CHARS = 60000
 DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 DEFAULT_ENCODING = "utf-8"
+MAX_BYTES = 3900  # si columna es VARCHAR2 en SQL
