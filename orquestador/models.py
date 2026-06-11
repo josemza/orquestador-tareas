c98832a45dfbf3ccadc6d90a0447cadd06859640
josemza <josemza27@gmail.com>
2026-06-11 13:28:23 -0500

    Refactor models to use NamedTuple and add GeneralConfig

--- orquestador/models.py
+++ orquestador/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import NamedTuple, Optional
 
 @dataclass(frozen=True)
 class StageResult:
@@ -21,6 +21,18 @@ class StageConfig:
     timeout: Optional[int] = None
     result_key: Optional[str] = None
     condicion: Optional[str] = None
+    allow_error: Optional[bool] = False
     query_sql: Optional[str] = None
     query_title: Optional[str] = None
     max_rows: int = 20
+
+@dataclass(frozen=True)
+class GeneralConfig:
+    log_file: str
+    path_log_summ: str
+    process_name: str
+    dev_mode: Optional[bool] = False
+
+class ConfigTotal(NamedTuple):
+    stages: list[StageConfig]
+    general: GeneralConfig
